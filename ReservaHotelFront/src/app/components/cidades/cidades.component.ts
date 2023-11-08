96ebfbe36cc3a8718f7e473abe1f4b9f60fc6bd9
Y4nnLS <101208372+Y4nnLS@users.noreply.github.com>
2023-11-08 13:02:31 -0300

    Atualização

--- ReservaHotelFront/src/app/components/cidades/cidades.component.ts
+++ ReservaHotelFront/src/app/components/cidades/cidades.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Cidade } from 'src/app/Cidade'; // Certifique-se de importar a classe Cidade correta
 import { CidadesService } from 'src/app/cidades.service'; // Certifique-se de importar o serviço de cidades correto
+import { Hotel } from 'src/app/Hotel'; // Certifique-se de importar a classe Hotel correta
+import { HoteisService } from 'src/app/hotel.service'; // Certifique-se de importar o serviço de hotéis correto
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-cidades',
@@ -11,11 +14,19 @@ import { CidadesService } from 'src/app/cidades.service'; // Certifique-se de im
 export class CidadesComponent implements OnInit {
   formularioCidades: any;
   tituloFormulario: string = '';
+  hoteis: Array<Hotel> | undefined;
 
-  constructor(private cidadesService: CidadesService) { }
+  constructor(private cidadesService: CidadesService,
+              private hoteisService: HoteisService) { }
 
   ngOnInit(): void {
     this.tituloFormulario = 'Nova Cidade';
+    this.hoteisService.listar().subscribe(hoteis => {
+      this.hoteis = hoteis;
+      if (this.hoteis && this.hoteis.length > 0){
+        this.formularioCidades.get('hotelId')?.setValue(this.hoteis[0].idHotel);
+      }
+    });
     this.formularioCidades = new FormGroup({
       Nome: new FormControl(null),
       Estado: new FormControl(null),
@@ -27,8 +38,20 @@ export class CidadesComponent implements OnInit {
   enviarFormulario(): void {
     console.log('Método enviarFormulario() chamado.');
     const cidade: Cidade = this.formularioCidades.value;
-    this.cidadesService.cadastrar(cidade).subscribe(result => {
-      alert('Cidade inserida com sucesso.');
-    });
-  }
+    const observer: Observer<Hotel> = {
+        next(_result): void{
+          alert('Modelo salvo com sucesso.');
+        },
+        error(_error): void {
+          alert('Erro ao salvar!');
+        },
+        complete(): void {
+        },
+        };
+      if (cidade.idCidade && !isNaN(Number(cidade.idCidade))){
+        this.cidadesService.atualizar(cidade).subscribe(observer);
+      } else{
+        this.cidadesService.cadastrar(cidade).subscribe(observer);
+      }
+    }
 }
